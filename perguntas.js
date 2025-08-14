69b61d6ddc74010ac5a33f654fa447f5d3b68d96
Felipe4192 <ramos.silva.felipe1410@escola.pr.gov.br>
2025-08-14 15:32:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Biologia',
+    'Qual é o maior órgão do corpo humano',
+    'A pele',
+)
+
+criaCartao(
+    'Biologia',
+    'Quantas vértebras compõem a coluna vertebral?',
+    '33 vértebras.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o músculo responsável pela respiração?',
+    'O diafragma.',
+)
+
+criaCartao(
+    'Biologia',
+    'Onde está localizado o coração no corpo humano?',
+    'No tórax, entre os pulmões.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o nome do osso que forma a testa?',
+    'Osso frontal.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a principal função dos glóbulos vermelhos?',
+    'Transportar oxigênio dos pulmões para os tecidos do corpo.',
+)
+
+criaCartao(
+    'Biologia',
+    'Quais ossos compõem o crânio?',
+    'Ossos do neurocrânio e viscerocrânio.',
+)
+
+criaCartao(
+    'Biologia',
+    'Onde se localizam os alvéolos pulmonares?',
+    'Nos pulmões, dentro dos bronquíolos.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual músculo é conhecido como o "músculo do abdômen"?',
+    'O reto abdominal.',
+)
